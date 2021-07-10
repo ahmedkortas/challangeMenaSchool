a0d4419bc2b992544f5c5e1596494743e6a53ee0
Mahmoud-Nwiry <mamo992001@gmail.com>
2021-07-10 14:40:27 +0300

    Finished Ex.2 (edit)

--- ex2.js
+++ ex2.js
@@ -12,3 +12,15 @@ let array = [15,6,[5,1],[10,[20,[0]]]];
 array = array.flat(Infinity);
 array.includes(0);
 array.includes(-1);
+
+
+// uncomplete
+//
+// let search = (array) => {
+//     for(let x = 0; x > array.length; x++){
+//         if(Array.isArray(array[x])) {
+//             array[x] = search(array[x]);
+//         }
+//     }
+//     return array;
+// }
