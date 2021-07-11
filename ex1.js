63a411961dc9316422d1262381d1846bc0cd69a7
ashjanraja <ashjannairat787@gmail.com>
2021-07-11 23:56:05 +0300

    add solution ex1

--- ex1.js
+++ ex1.js
@@ -8,9 +8,22 @@ exp
  
  */
 
+
 function reverseNestedArray(array) {
-    //TODO
+  //TODO
+    for (let i = array.length - 1; i >= 0; i--) {
+      if (typeof array[i] === "object") {
+        reverseNestedArray(array[i]);
+      }
+    }
+    array.reverse();
+    return array;
 }
+let arr1 = [9, [5, [10, 30]], 6];
+// let arr1 = [1, [2, 3], 4];
+// let arr1 = [1, 2, 3, 4];
+let reverseArray = reverseNestedArray(arr1);
+console.log(reverseArray);
 
 
 module.exports = reverseNestedArray;
